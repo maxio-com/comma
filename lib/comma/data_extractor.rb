3ffdc3d8c052fe14720f05e6ef37bbb9981733a4
Marcelo De Polli and David Cole <pair+mp+dc@chargify.com>
2016-01-19 17:49:15 -0200

    Rails 4-related changes
    * Update gem requirements for Rails 4.0
    * Use #is_a?(DateTime) to determine whether or not to use time zone.

--- lib/comma/data_extractor.rb
+++ lib/comma/data_extractor.rb
@@ -38,7 +38,7 @@ module Comma
       end
 
       def time_zone_adjusted_value(output)
-        if output.respond_to?(:in_time_zone) && extraction_object.respond_to?(:current_time_zone) && extraction_object.current_time_zone
+        if output.is_a?(DateTime) && extraction_object.respond_to?(:current_time_zone) && extraction_object.current_time_zone
           output.in_time_zone(extraction_object.current_time_zone)
         else
           output
