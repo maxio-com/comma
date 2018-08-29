ac74e7043bf67a1430bb7beb164ed56cdf748c2f
Drew Blas <drew.blas@gmail.com>
2018-08-29 15:13:05 -0500

    Replace spec file secret token with placeholder value
    People are confused in thinking it's a real secret token used somewhere
    
    It's not

--- spec/rails_app/rails_app.rb
+++ spec/rails_app/rails_app.rb
@@ -6,7 +6,7 @@ require 'action_view/railtie'
 require 'rails_app/active_record/config' if defined?(ActiveRecord)
 
 app = Class.new(Rails::Application)
-app.config.secret_token = '6f6acf0443f74fd0aa8ff07a7c2fbe0a'
+app.config.secret_token = 'notarealsecrettoken'
 app.config.session_store :cookie_store, :key => '_rails_app_session'
 app.config.active_support.deprecation = :log
 app.config.eager_load = false
